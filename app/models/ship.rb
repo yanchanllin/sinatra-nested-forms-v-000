999958363da5970d33bd881e2cddd8d32a2c308f
YanC Lin <yonalin1@gmail.com>
2018-03-20 19:30:06 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,21 +1,21 @@
 class Ship
   attr_accessor :name, :type, :booty
 
-@@all = []
+  @@all = []
 
-def initialize(name, type, booty)
-  @name = name
-  @type = type
-  @booty = booty
-  @@all << self
-end
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
 
-def self.clear
-  @@all.clear
-end
+  def self.clear
+    @@all.clear
+  end
 
 end
